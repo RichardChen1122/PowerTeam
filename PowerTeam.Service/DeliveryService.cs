e31de60045a5815d07c779f952814eccddaeb2c3
RichardChen1122 <rchen89@126.com>
2018-06-14 08:18:27 +0000

    Add files via upload

---
+++ PowerTeam.Service/DeliveryService.cs
@@ -0,0 +1,19 @@
+﻿using PowerTeam.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerTeam.Service
+{
+    public class DeliveryService : BaseService, IDeliveryService
+    {
+        public DeliveryService(IDataContext dataContext) : base(dataContext)
+        {
+        }
+
+        public void TestMethod()
+        {
+            PTDataContext.Delivery.GetAll();
+        }
+    }
+}
